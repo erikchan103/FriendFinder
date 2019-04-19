b92a84395ae7156955ebee8f7573e1e977c8bfae
erikchan103 <erik.chan@sugarmade.com>
2019-04-19 00:39:42 -0700

    finished project

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        name:"Erik",
+        photo:"#",
+        scores:[
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
